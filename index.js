6d58329e12a6f1b9d4c1bbf26d4ded9398594735
silvana gutierrez <silvana87@users.noreply.github.com>
2021-03-13 13:04:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const express  = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public/'));
+app.listen('3000', function () {
+    console.log("Servidor iniciado escuchando el puerto 3000");
+}   )
